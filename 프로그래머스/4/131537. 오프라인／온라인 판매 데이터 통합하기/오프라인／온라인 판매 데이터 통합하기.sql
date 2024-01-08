c5b183bf99c2c05a62422b33d823a9e50b1dc7ad
cheol2Y <113869397+cheol2Y@users.noreply.github.com>
2024-01-08 20:24:52 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,26 @@
+# select 
+#     DATE_FORMAT(t1.SALES_DATE,"%Y-%m-%d") as SALES_DATE,
+#     t1.product_id,
+#     t1.user_id as user_id
+# from online_sale t1
+# join
+#     (select DATE_FORMAT(SALES_DATE,"%Y-%m-%d") as SALES_DATE, PRODUCT_ID, SALES_AMOUNT
+#     from OFFLINE_SALE) t2
+# on t1.PRODUCT_ID = t2.PRODUCT_ID
+# where (t1.Sales_date like "2022-03%" or t2.Sales_date like "2022-03%")
+# group by t1.SALES_DATE, t2.SALES_DATE
+# order by t1.SALES_DATE
+
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+FROM ONLINE_SALE
+WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+UNION ALL
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT
+FROM OFFLINE_SALE
+WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+
+
+
+# 3월
+# 판매 날짜, 상품ID, 유저ID, 판매량
