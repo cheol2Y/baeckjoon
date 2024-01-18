1bd9376ff0f613bdeb464455bbba9236c4e103f5
cheol2Y <113869397+cheol2Y@users.noreply.github.com>
2024-01-18 16:46:46 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,15 +1,17 @@
 # select 
-#     DATE_FORMAT(t1.SALES_DATE,"%Y-%m-%d") as SALES_DATE,
+#     DATE_FORMAT(t1.SALES_DATE,"%Y-%m-%d") as SALES_DATE, 
 #     t1.product_id,
-#     t1.user_id as user_id
+#     t1.user_id as user_id,
+#     sum(t1.SALES_AMOUNT + t2.SALES_AMOUNT) as SALES_AMOUNT
 # from online_sale t1
 # join
-#     (select DATE_FORMAT(SALES_DATE,"%Y-%m-%d") as SALES_DATE, PRODUCT_ID, SALES_AMOUNT
+#     (select DATE_FORMAT(SALES_DATE,"%Y-%m-%d") as SALES_DATE, 
+#      PRODUCT_ID, Null as user_id, SALES_AMOUNT
 #     from OFFLINE_SALE) t2
 # on t1.PRODUCT_ID = t2.PRODUCT_ID
 # where (t1.Sales_date like "2022-03%" or t2.Sales_date like "2022-03%")
 # group by t1.SALES_DATE, t2.SALES_DATE
-# order by t1.SALES_DATE
+# ORDER BY t1.SALES_DATE, t1.PRODUCT_ID, t1.USER_ID
 
 SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
 FROM ONLINE_SALE
