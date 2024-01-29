ec188e1fca55359315d0a4c0e1ad9b6e82f93f36
cheol2Y <113869397+cheol2Y@users.noreply.github.com>
2024-01-29 20:59:11 +0900

    Update 이진 변환 반복하기.py

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,30 +1,48 @@
+# def solution(s):
+#     count1 = 0
+#     count2 = 0
+#     while s != 1:
+#         count1 += 1
+        
+#         #1로 split 해서 안에 0 의 갯수 확인 가능
+#         for i in list(s):
+#             if i == '0':
+#                 count2 += 1
+        
+#         # count는 했으니 이제 0을 제외하여 변환
+#         s = s.replace('0', '')
+
+#         if s =='1':
+#           break
+#         result = []
+#         s = len(s)
+#         while s//2 != 0:
+#           result.append(s%2)
+#           s= s//2
+#         result.append(1)
+#         change = ''
+#         for i in range(len(result)):
+#             change = change + str(result[-(i+1)])
+
+#         s=change
+
+#     answer = [count1, count2]
+#     return answer 
+
+
 def solution(s):
     count1 = 0
     count2 = 0
-    while s != 1:
+    while s != '1':
         count1 += 1
-        
-        #1로 split 해서 안에 0 의 갯수 확인 가능
         for i in list(s):
-            if i == '0':
+            if i == '0' :
                 count2 += 1
-        
-        # count는 했으니 이제 0을 제외하여 변환
-        s = s.replace('0', '')
+        #if s == 
+        s= s.replace('0', '')
 
-        if s =='1':
-          break
-        result = []
-        s = len(s)
-        while s//2 != 0:
-          result.append(s%2)
-          s= s//2
-        result.append(1)
-        change = ''
-        for i in range(len(result)):
-            change = change + str(result[-(i+1)])
+        s = format(len(s), 'b')
 
-        s=change
-
+    
     answer = [count1, count2]
-    return answer 
+    return answer
