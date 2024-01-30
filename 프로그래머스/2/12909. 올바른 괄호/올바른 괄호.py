dda9a790f7c8e5199254fd58dfa3c8354d63a51b
cheol2Y <113869397+cheol2Y@users.noreply.github.com>
2024-01-30 13:30:50 +0900

    [level 2] Title: 올바른 괄호, Time: 6.63 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
