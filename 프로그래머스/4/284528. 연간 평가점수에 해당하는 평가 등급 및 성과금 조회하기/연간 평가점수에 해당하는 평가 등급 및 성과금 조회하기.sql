1365cbc2a49c4b6811851c6ef775d61bc9e2db1a
윤성철 <113869397+cheol2Y@users.noreply.github.com>
2024-04-19 13:49:07 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,11 +1,12 @@
 WITH TMP AS (
     SELECT 
         EMP_NO,
-        IF(AVG(SCORE) >= 96, 'S',
-            IF(AVG(SCORE) >= 90, 'A',
-                IF(AVG(SCORE) >= 80, 'B', 'C')
-            )
-        ) AS GRADE
+    
+        CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >=90 THEN 'A'
+        WHEN AVG(SCORE)>= 80 THEN 'B'
+        ELSE 'C'
+        END AS GRADE
     FROM HR_GRADE
     GROUP BY EMP_NO
 )
@@ -23,4 +24,3 @@ SELECT
 FROM HR_EMPLOYEES E
 JOIN TMP ON E.EMP_NO = TMP.EMP_NO
 ORDER BY EMP_NO ASC
-
