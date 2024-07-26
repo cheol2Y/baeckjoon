acefe91228dc73b20046a91f8bedbc129140c0ad
윤성철 <113869397+cheol2Y@users.noreply.github.com>
2024-07-26 15:58:47 +0900

    Update 년， 월， 성별 별 상품 구매 회원 수 구하기.sql

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -6,7 +6,7 @@ select
     count(distinct t2.USER_ID) as users
 from ONLINE_SALE t1 
 join user_info t2 
-on t1.USER_ID = t2.USER_ID
+on t1.USER_ID = t2.USER_ID 
 group by year, month, gender
 Having gender is not NULL
 order by year, month, gender asc
